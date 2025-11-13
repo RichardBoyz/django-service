a025e26aec2fce6a3dd32d433b610617860926f5
RichardBoyz <r1382333@gmail.com>
2025-11-13 10:29:33 +0800

    feat: add docker-compose setup for service

---
+++ python_django_challenge_template/src/backend-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Running database migrations..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+echo "Starting Django server..."
+python manage.py runserver 0.0.0.0:8000
